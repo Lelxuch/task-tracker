69cab1f7e0e2e034000c9605455306336d706924
Lelxuch <nakyp.sanzhar@gmail.com>
2021-07-08 12:33:42 +0600

    .

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,20 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [{
+  id: 1,
+  text: "Go to doctor",
+  day: "10/27/2021",
+  reminder: false
+}, 
+{
+  id: 2,
+  text: "Check the car volume",
+  day: "10/29/2021",
+  reminder: true
+},
+{
+  id: 3,
+  text: "Finish conclusion",
+  day: "12/22/2021",
+  reminder: true
+}];
